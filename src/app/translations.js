b9f912b320a9edfb9f4ec59134bbd754eb15d012
katriyna <katriyna@gmail.com>
2020-11-04 01:51:00 +0300

    lint happy

--- src/app/translations.js
+++ src/app/translations.js
@@ -3,17 +3,17 @@ import {setLocale} from 'hub-dashboard-addons/dist/localization';
 const translationFiles = require.context('./translations/', true, /\.po$/);
 
 const translations = translationFiles.keys().
-reduce((result, fileKey) => {
-  const lang = fileKey.split('.po')[0].split('_')[1];
-  const fileJson = translationFiles(fileKey);
-  result[lang] = Object.keys(fileJson).
-  reduce(
-    (accumulator, propertyKey) =>
-      ({...accumulator, ...fileJson[propertyKey]}),
-    {}
-  );
-  return result;
-}, {});
+  reduce((result, fileKey) => {
+    const lang = fileKey.split('.po')[0].split('_')[1];
+    const fileJson = translationFiles(fileKey);
+    result[lang] = Object.keys(fileJson).
+      reduce(
+        (accumulator, propertyKey) =>
+          ({...accumulator, ...fileJson[propertyKey]}),
+        {}
+      );
+    return result;
+  }, {});
 
 export function initTranslations(locale) {
   if (translations[locale]) {
